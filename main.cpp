ca454c11687e12db289666a00ee93a23bc172d4d
Gerald Freislich <freislichg@gmail.com>
2022-07-28 09:30:16 +0930

    safe

--- main.cpp
+++ main.cpp
@@ -1,21 +1,7 @@
 #include <iostream>
-// #include "human.h"
-// #include "player.h"
-// #include "computer.h"
-// #include "referee.h"
 #include "game_manager.h"
 
 int main() {
-    // Player *user = new Human();
-    // Player *computer = new Computer();
-    // Referee ref;
-
-    // user->choose_move();
-    // computer->choose_move();
-
-    // ref.check_winner(user, computer);
-    // std::cout << "\n" << "you played: " << user->get_move() << " The computer played: " << computer->get_move() << "\n";
-    // std::cout << "The winner is: "<<ref.get_winner() << "\n";
     GameManager gamecontroller;
     gamecontroller.main_loop();
 }
