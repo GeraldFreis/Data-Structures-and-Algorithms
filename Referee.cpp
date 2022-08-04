545a62ffcc0bcd0febbe7614d61992996044108d
Gerald Freislich <freislichg@gmail.com>
2022-08-05 00:11:29 +0930

    safe

--- Referee.cpp
+++ Referee.cpp
@@ -3,6 +3,12 @@
 Referee::Referee(){
 }
 
+/* refGame function requires Human, Computer parameters (returns a character of who won the game)
+Returns T if the game was a tie
+Returns L if the player lost
+Returns W if the player won
+Returns A if a different output was produced
+*/
 char Referee::refGame(Human player1, Computer player2){
     char playermove = player1.make_move();
     char computermove = player2.make_move();
