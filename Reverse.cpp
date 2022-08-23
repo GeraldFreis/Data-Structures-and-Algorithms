5bfa0d5125ab9932e4c1641af9e50487f9f13372
Gerald Freislich <freislichg@gmail.com>
2022-08-23 14:18:31 +0930

    safe

---
+++ Reverse.cpp
@@ -0,0 +1,9 @@
+#include "Reverse.h"
+
+int Reverse::reverseInt(int string_to_reverse){
+
+};
+
+std::string Reverse::reverseString(std::string reverse_string){
+    
+};
---
+++ Reverse.h
@@ -0,0 +1,12 @@
+#ifndef REVERSE_H
+#define REVERSE_H
+#include <string>
+
+// Reversing recursively class
+class Reverse {
+    public:
+        std::string reverseString(std::string reversable_string);
+        int reverseInt(int reverse_int);
+};
+
+#endif
