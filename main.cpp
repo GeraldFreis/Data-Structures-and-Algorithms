fe332ee513c9ea5b3653a680f5b2495ff6850ec3
Gerald Freislich <freislichg@gmail.com>
2022-08-29 13:50:14 +0930

    changes

--- main.cpp
+++ main.cpp
@@ -16,19 +16,22 @@
 
 
 int main(){
-    std::vector<int> userints({6, -11, 53, -16, 73, 128, 105, 104, -71, -179, 102, 12, 21, -145, -99, 199,-156, -186, 43, -189});
-    // for(int i = 0; i < 20; i++){
-    //     int x;
-    //     std::cin >> x;
-    //     userints.push_back(x);
-    // }
+    std::vector<int> userints;
+    for(int i = 0; i < 20; i++){
+        int x;
+        std::cin >> x;
+        userints.push_back(x);
+    }
 
     MapGeneric *maptrip = new MapTriple; MapGeneric *mapabs = new MapAbsoluteValue;
     FilterGeneric *filtertwodigs = new FilterForTwoDigitPositive; FilterGeneric *filterodds = new FilterOdd;
     ReduceGeneric *reducemin = new ReduceMinimum; ReduceGeneric *reducegcd = new ReduceGCD;
 
-    std::vector<int> maps = mapabs->map(maptrip->map(userints));
-    std::vector<int> filters = filtertwodigs->filter(filterodds->filter(maps));
-
+    std::vector<int> maps = maptrip->map(mapabs->map(userints));
+    for(auto a: maps){std::cout << a << " ";}
+    std::cout << "\n";
+    std::vector<int> filters = filterodds->filter(filtertwodigs->filter(maps));
+    for(auto a: filters){std::cout << a << " ";}
+    std::cout << "\n";
     std::cout << reducemin->reduce(filters) << " " << reducegcd->reduce(filters) << "\n";
 }
