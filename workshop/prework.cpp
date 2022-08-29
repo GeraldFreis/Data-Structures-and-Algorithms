9f1fef567f988a22b03e64c9f380fa05b3e93b21
Gerald Freislich <freislichg@gmail.com>
2022-08-29 10:11:28 +0930

    safe

---
+++ workshop/prework.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+int computingxn(int x, int n){
+    if(n != 0){
+        return (x*computingxn(x, n-1));
+    }
+    else{
+        return 1;
+    }
+
+}
+
+int main(){
+    std::cout << computingxn(5, 2) << "\n";
+}
