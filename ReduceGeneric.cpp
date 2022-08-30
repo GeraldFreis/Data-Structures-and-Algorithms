37f90c318b5171d5eed27e02be9a41143ba3dea8
Gerald Freislich <freislichg@gmail.com>
2022-08-30 17:23:56 +0930

    safe

--- ReduceGeneric.cpp
+++ ReduceGeneric.cpp
@@ -4,14 +4,16 @@
 
 int ReduceGeneric::reduce(std::vector<int> given_vector){
     if(given_vector.size() > 2){
-        for(int i = 1; i < given_vector.size()-1; i++){
-            if(given_vector.at(0) != given_vector.at(i)){
-                int result = binaryOperator(given_vector.at(0), given_vector.at(i));
-                if(result > returnable_res){returnable_res = result;}
-            }
+        int result = binaryOperator(given_vector.at(0), given_vector.at(given_vector.size()-1));
+        // std::cout << result << " " << '\n';
+        // for(auto a: given_vector){std::cout << "*" << a << " ";} 
+        if(result == given_vector.at(given_vector.size()-1)){ // if element 0 > element len -1
+            given_vector.erase(given_vector.begin());
+        } 
+        else{
+            given_vector.pop_back();
         }
-        given_vector.erase(given_vector.begin());
-        // iterating recurisvely through the rest of the function
+        // iterating recurisvely through the rest of the vector
         reduce(given_vector);
     }
     return binaryOperator(given_vector.at(0), given_vector.at(1));
