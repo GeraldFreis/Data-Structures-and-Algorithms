91ded110ff5995301d34a3666436e70b7fc6c87a
Gerald Freislich <freislichg@gmail.com>
2022-08-30 16:53:15 +0930

    safe

--- ReduceGCD.cpp
+++ ReduceGCD.cpp
@@ -1,5 +1,5 @@
 #include "ReduceGCD.h"
-#include <cmath>
+#include <iostream>
 
 int ReduceGCD::binaryOperator(int x, int y){
     if(y != 0){
@@ -14,18 +14,18 @@ ReduceGCD::ReduceGCD(){gcd = 0;}
 
 int ReduceGCD::reduce(std::vector<int> given_vector){
     if(given_vector.size() > 2){
-        for(auto val: given_vector){
-            int result = binaryOperator(given_vector.at(0), val);
-            if(result > gcd){gcd = result;}
+        for(int i = 1; i < given_vector.size()-1; i++){
+            if(given_vector.at(i) != given_vector.at(0)){
+                int result = binaryOperator(given_vector.at(0), given_vector.at(i));
+                if(result > gcd){gcd = result;}
+                // std::cout << given_vector.at(i) << " " << given_vector.at(0) << " " << result << " " << gcd <<"\n";
+            }
         }
 
         // deleting the first element as that is what we have been comparing this with
         given_vector.erase(given_vector.begin());
         // iterating recurisvely through the rest of the vector
         reduce(given_vector);
-        
-
     }
-    return binaryOperator(given_vector.at(0), given_vector.at(1));
-
+    return gcd;
 };
