638d352be03c451e40f67eb1c0e5868e6a0d4d4f
Gerald Freislich <freislichg@gmail.com>
2022-08-30 17:07:22 +0930

    safe

--- ReduceMinimum.cpp
+++ ReduceMinimum.cpp
@@ -4,3 +4,16 @@ int ReduceMinimum::binaryOperator(int x, int y){
     if(x < y){return x;}
     else {return y;}
 }
+
+int ReduceMinimum::reduce(std::vector<int> given_vector){
+    if(given_vector.size() <= 2){
+        return binaryOperator(given_vector.at(0), given_vector.at(1));
+    }
+    else{
+        int result = binaryOperator(given_vector.at(0), given_vector.at(1));
+        if(result == given_vector.at(0)){given_vector.erase(given_vector.begin());}
+        else{given_vector.erase(given_vector.begin()+1);}
+
+        reduce(given_vector);
+    }
+}
--- ReduceMinimum.h
+++ ReduceMinimum.h
@@ -6,6 +6,9 @@
 class ReduceMinimum : public ReduceGeneric {
     private:
         int binaryOperator(int x, int y);
+    
+    public:
+        int reduce(std::vector<int> given_vector);
 };
 
 #endif
