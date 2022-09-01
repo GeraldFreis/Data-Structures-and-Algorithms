de16555ff6b99d45ca01230473c6a83a8014855a
Gerald Freislich <freislichg@gmail.com>
2022-09-01 22:13:55 +0930

    changes

--- ReduceMinimum.h
+++ ReduceMinimum.h
@@ -6,8 +6,9 @@
 class ReduceMinimum : public ReduceGeneric {
     private:
         int binaryOperator(int x, int y);
-    
+        int min;
     public:
+        ReduceMinimum();
         int reduce(std::vector<int> given_vector);
 };
 
