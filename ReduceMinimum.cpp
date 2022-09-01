b3281fa575e3d00551d121169d7397becd2ebbc4
Gerald Freislich <freislichg@gmail.com>
2022-09-01 22:18:21 +0930

    changes

--- ReduceMinimum.cpp
+++ ReduceMinimum.cpp
@@ -11,12 +11,14 @@ ReduceMinimum::ReduceMinimum(){
 
 int ReduceMinimum::reduce(std::vector<int> given_vector){
     if(given_vector.size() > 2){
-        int result = binaryOperator(given_vector.at(0), given_vector.at(1));
+        if(given_vector.at(0) != given_vector.at(1)){
+            int result = binaryOperator(given_vector.at(0), given_vector.at(1));
 
-        if(result == given_vector.at(0)){given_vector.erase(given_vector.begin());}
-        else if(result == given_vector.at(1)){given_vector.erase(given_vector.begin()+1);}
+            if(result == given_vector.at(0)){given_vector.erase(given_vector.begin());}
+            else if(result == given_vector.at(1)){given_vector.erase(given_vector.begin()+1);}
 
-        min = reduce(given_vector);
+            min = reduce(given_vector);
+        }
     }
 
     if(binaryOperator(given_vector.at(0), given_vector.at(1) < min)){
