6266370208d38161617a2f0b805375b5937cbc16
Gerald Freislich <freislichg@gmail.com>
2022-09-01 22:09:05 +0930

    changes

--- ReduceMinimum.cpp
+++ ReduceMinimum.cpp
@@ -10,13 +10,15 @@ int ReduceMinimum::reduce(std::vector<int> given_vector){
         int result = binaryOperator(given_vector.at(0), given_vector.at(given_vector.size()-1));
 
         if(result == given_vector.at(given_vector.size()-1)){ // if element 0 > element len -1
-            given_vector.erase(given_vector.begin());
+            // given_vector.erase(given_vector.begin());
+            given_vector.pop_back();
         } 
         else {
-            given_vector.pop_back();
+            given_vector.erase(given_vector.begin())
         }
         // iterating recurisvely through the rest of the vector
         reduce(given_vector);
     }
+
     return binaryOperator(given_vector.at(0), given_vector.at(1));
 }
