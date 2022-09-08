2f1b52b4aea9ad6225c3dbffa1e2522c97427c08
Gerald Freislich <freislichg@gmail.com>
2022-09-08 14:35:27 +0930

    changes

---
+++ Rearrange.cpp
@@ -0,0 +1,14 @@
+#include "Rearrange.h"
+
+Individual Rearrange::mutate(Individual i, int k){
+    if(k > i.getLength()){k = i.getLength() - k;}
+
+    std::string list_pre_k; std::string list_post_k_inc_k;
+    std::string full_list = i.getString();
+
+    for(int x = 0; x < k; x++){list_pre_k += full_list[x];}
+    for(int x = k; x < i.getLength(); x++){list_post_k_inc_k += full_list[x];}
+
+    Individual new_indie(list_post_k_inc_k + list_pre_k);
+    return new_indie;
+}
