a6432d894d34c0fd4ce9a6a2bae5c1b8cd40d7e5
Gerald Freislich <freislichg@gmail.com>
2022-09-09 17:56:06 +0930

    safe

--- Rearrange.cpp
+++ Rearrange.cpp
@@ -1,7 +1,10 @@
 #include "Rearrange.h"
 
 Individual Rearrange::mutate(Individual i, int k){
-    if(k > i.getLength()){k = i.getLength() - k;}
+    if(k > i.getLength()){
+        int dividend = k / i.getLength();
+        k = k - (dividend * i.getLength());
+    }
 
     std::string list_pre_k; std::string list_post_k_inc_k;
     std::string full_list = i.getString();
