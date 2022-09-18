d3c947727358b6b3ec794bf00a34249c30a457db
Gerald Freislich <freislichg@gmail.com>
2022-09-18 18:31:25 +0930

    cleaning

--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-extern int numDup(int arr[], int start, int size, int element);
-
-int main(){
-    int arr[] = {1, 3, 3, 5, 6};
-    std::cout << numDup(arr, 0, 5, 3) << "\n";
-}
