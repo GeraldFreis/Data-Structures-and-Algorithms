ba2c49282c2e1b1d0830d9c832e0a27c4003aca1
Gerald Freislich <freislichg@gmail.com>
2022-10-11 17:18:35 +1030

    safe

--- main.cpp
+++ main.cpp
@@ -5,44 +5,57 @@
 
 
 int main(){
-    LinkedList *list = new LinkedList;
-    int given_val;
-    int counter = 0;
-    while(std::cin >> given_val){
-        if(counter == 0){list->addFront(given_val); counter++;}
-        else {list->addEnd(given_val);}
-        // std::cout << given_val << "\n";
+    int *given_array = new int[100]; int param1, param2, counter = 0;
+    std::string command_string; std::string raw_input_string;
+    std::getline(std::cin, raw_input_string); // reading in the data
+    // command should always be 5 and 6 from the end
+    
+    if(raw_input_string[raw_input_string.size() - 6] != 'S' && raw_input_string[raw_input_string.size()-5] != 'S'){
+        command_string += raw_input_string[raw_input_string.size()-6];
+        command_string += raw_input_string[raw_input_string.size()-5];
+    } else {
+        command_string += "S";
     }
 
-    std::string command_string;
-    std::cin >> command_string;
-    
-    int other_val_1, other_val_2;
+
+
+    for(int i = 0; i < raw_input_string.size()-7; i++){
+        if(raw_input_string[i] == ' '){continue;}
+        else {
+            if(raw_input_string[i+1] != ' ' && raw_input_string[i+1] <= 57){ 
+                std::string newstring; 
+                newstring += raw_input_string[i];
+                newstring+= raw_input_string[i+1];
+                // std::cout << newstring << "\n";
+                given_array[counter] = std::stoi(newstring); counter++;
+                i++;
+            }
+            else if(raw_input_string[i+1] == ' '){
+                // std::cout << raw_input_string[i] << "\n";
+                given_array[counter] = raw_input_string[i] - '0'; counter++;
+            }
+        }
+    }
+    // std::cout << command_string << "\n";
+    param1 = raw_input_string[raw_input_string.size()-3]-'0'; param2 = raw_input_string[raw_input_string.size()-1] - '0';
+    // std::cout << param1 << " " << param2 << "\n";
+    LinkedList *list =  new LinkedList(given_array, counter);
     if(command_string == "AF"){
-        std::cin >> other_val_1;
-        list->addFront(other_val_1);
+        list->addFront(param1);
     } else if(command_string == "AE"){
-        std::cin >> other_val_1;
-        list->addEnd(other_val_1);
+        list->addEnd(param1);
     } else if(command_string == "AP"){
-        int position;
-        std::cin >> other_val_1 >> other_val_2;
-        std::cout << "\n" << other_val_1 << " " << other_val_2 << "\n";
-        list->addAtPosition(other_val_2, other_val_1);
+        list->addAtPosition(param1, param2);
     } else if(command_string == "S"){
-        std::cin >> other_val_1;
-        list->search(other_val_1);
+        list->search(param1);
     } else if(command_string == "DF"){
         list->deleteFront();
     } else if(command_string == "DE"){
         list->deleteEnd();
     } else if(command_string == "DP"){
-        std::cin >> other_val_1;
-        list->deleteAtPosition(other_val_1);
+        list->deleteAtPosition(param1);
     } else if(command_string == "GI"){
-        std::cin >> other_val_1;
-        std::cout << "\n" << other_val_1 << "\n";
-        list->getItem(other_val_1);
+        list->getItem(param1);
     }
     list->printItems();
 }
