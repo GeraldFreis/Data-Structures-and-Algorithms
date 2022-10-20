41fdd2376d39ec2e91608f22a4426e40a931fa5b
Gerald Freislich <freislichg@gmail.com>
2022-10-20 12:30:25 +1030

    safe

---
+++ InterpretingEquation.cpp
@@ -0,0 +1,11 @@
+#include "InterpretingEquation.h"
+#include <iostream>
+
+
+void InterpretingEquation::calculate_equation(char *command_array, int command_amount, int *numbers, int numbers_amount){
+    // we take the command array, and then check size, and compute equation
+};
+
+void InterpretingEquation::print_equation(){
+    std::cout << equation;
+}
--- InterpretingEquation.h
+++ InterpretingEquation.h
@@ -6,7 +6,6 @@ class InterpretingEquation {
     private:
         std::string equation;
     public:
-        InterpretingEquation();
         void calculate_equation(char *command_array, int command_amount, int *numbers, int numbers_amount);
         void print_equation();
 };
